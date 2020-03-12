e2a459a2dedc5763b08a6c5a10f1285eb53be70f
Courtney Schild <courtneyschild@gmail.com>
2020-03-11 18:16:30 -0700

    align content and styles between screens.

--- src/screens/Account.js
+++ src/screens/Account.js
@@ -3,13 +3,33 @@ import { StyleSheet, View, Text, StatusBar } from 'react-native';
 
 export default function Account() {
   return (
-    <View>
+    <View style={styles.container}>
       <StatusBar barStyle="dark-content" backgroundColor="#fff" />
-      <Text>Your account</Text>
+      <Text style={styles.smallLink}>Sign Out</Text>
+      <Text style={styles.header}>Your account</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-
+  container: {
+    backgroundColor: '#fff',
+    paddingTop: 20,
+    paddingLeft: 32,
+    paddingRight: 32,
+    flex: 1,
+  },
+  smallLink: {
+    marginTop: 12,
+    fontFamily: 'poppins-semibold',
+    fontSize: 12,
+    textAlign: 'right',
+  },
+  header: {
+    marginTop: 12,
+    marginBottom: 16,
+    fontFamily: 'playfairdisplay-regular',
+    fontSize: 36,
+    textAlign: 'center',
+  }
 });
